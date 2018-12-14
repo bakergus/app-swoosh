e7fc22e5d73c2ec73fcc38b51ab0a303b7db0558
Gus <xiaoxian.duan@gmail.com>
2018-12-14 11:13:38 +0800

    created swoosh app project

---
+++ swoosh/swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Xiaoxian Duan on 12/14/18.
+//  Copyright © 2018 Xiaoxian Duan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
