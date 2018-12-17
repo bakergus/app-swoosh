a6ab54e4be9d874ed194bdfdc41fa22af0c85ce7
Gus <xiaoxian.duan@gmail.com>
2018-12-17 18:10:03 +0800

    data model added

---
+++ swoosh/swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Xiaoxian Duan on 12/16/18.
+//  Copyright © 2018 Xiaoxian Duan. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
